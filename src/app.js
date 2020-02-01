cbfb35b2f14ac95f3a92d726cf99187c6d6a6fc2
Michelle Van <mjtvan@gmail.com>
2020-02-01 02:27:47 +0000

    partials headers/footers

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const path = require("path");
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+
+//Define paths for Express configurations
 const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, '../templates/partials')
 
+//Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -17,22 +26,26 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        aboutme: 'This is about me'
+        title: 'About Me',
+        name: "Miche"
     })
 })
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        helpMessage: 'If you need any help, please contact us'
-    })
-})
+app.get("/help", (req, res) => {
+    res.render("help", {
+        helpMessage: "If you need any help, please contact us",
+        title: "Help",
+        name: "Mich"
+    });
+});
 
-app.get('/weather', (req, res) => {
+app.get("/weather", (req, res) => {
     res.send({
-        forecast: 'Sunny',
-        location: 'Philadelphia'
-    })
-})
+        forecast: "Sunny",
+        location: "Philadelphia"
+    });
+});
+
 
 app.listen(3000, () => {
     console.log('Server has started on PORT 3000')
