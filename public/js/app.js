e05b80d784160ee2199969c7559d07c03e14cc1c
Michelle Van <mjtvan@gmail.com>
2020-02-02 00:15:35 +0000

    fetch front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,18 @@
 //client side js
 console.log("Client side js script is loaded")
+
+fetch("http://puzzle.mead.io/puzzle")
+    .then(res => res.json())
+    .then(data => console.log(data));
+
+const url = "http://localhost:3000/weather?address=!";
+
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location, data.forecast)
+        }
+    })
