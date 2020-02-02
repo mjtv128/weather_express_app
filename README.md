d72dbf8d0956e3d9ef0b595296b274b995468d8f
Michelle Van <mjtvan@gmail.com>
2020-02-02 00:49:07 +0000

    added client side

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# weather_express_app
+Weather App
+---
+
+run ```npm install``` then ```nodemon src/app.js```
+
+A web application that gives the weather forecast based on your location input.
+
+The front end in the browser asks for the user to provide a location.
+Node js communicates with 2 external APIs : a location based API (Mapbox) and a real time weather API (Dark Sky) to convert the user's location to a weather forecast. 
+
+The server side sends forecast back to browser so data is rendered for user to see. 
+
--- public/js/app.js
+++ public/js/app.js
@@ -3,18 +3,25 @@ console.log("Client side js script is loaded")
 
 const weatherForm = document.querySelector('form')
 const searchTerm = document.querySelector('input')
+const messageOne = document.querySelector('#message1')
+const messageTwo = document.querySelector("#message2");
+
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = searchTerm.value
 
+    messageOne.textContent = "Loading..."
+    messageTwo.textContent = ''
+
     fetch(`http://localhost:3000/weather?address=${location}`)
         .then(res => res.json())
         .then(data => {
             if (data.error) {
-                console.log(data.error);
+                messageOne.textContent = data.error
             } else {
-                console.log(data.location, data.forecast);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast
             }
         });
 })
