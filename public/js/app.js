8075f09fae95fce70da5c45c77f2f4df5d3243b6
Michelle Van <mjtvan@gmail.com>
2020-02-02 00:27:31 +0000

    added front end

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,20 @@
 //client side js
 console.log("Client side js script is loaded")
 
-fetch("http://puzzle.mead.io/puzzle")
-    .then(res => res.json())
-    .then(data => console.log(data));
+const weatherForm = document.querySelector('form')
+const searchTerm = document.querySelector('input')
 
-const url = "http://localhost:3000/weather?address=!";
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = searchTerm.value
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        if (data.error) {
-            console.log(data.error)
-        } else {
-            console.log(data.location, data.forecast)
-        }
-    })
+    fetch(`http://localhost:3000/weather?address=${location}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data.location, data.forecast);
+            }
+        });
+})
